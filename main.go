e88ed600dc4da89b9bf266a89aa0254d72d3bb04
codedbychavez <codedbychavez@gmail.com>
2022-03-07 19:40:47 -0400

    Included fiber app

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
 	"fmt"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+
+	// Create fiber app
+	app :=  fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	fmt.Println("Starting app")
+
+	app.Listen(":3000")
 }
