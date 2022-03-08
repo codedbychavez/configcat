b04cea4e10ebfe77623eae7438f91eb1da23991f
Chavez Harris <codedbychavez@gmail.com>
2022-03-08 19:28:34 -0400

    added function for generating public routes

---
+++ app/routing/public.go
@@ -0,0 +1,23 @@
+package routing
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"configcat-homework/app/controllers"
+)
+
+
+func CreatePublicRoutes(router fiber.Router, controllerManager controllers.ControllerManager) {
+	router.Use(func(c *fiber.Ctx) error {
+		c.Set("Allow", "GET, POST, PUT")
+		return c.Next()
+	})
+
+	// A status route to check that the router is OK
+	router.Get("/status", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	router.Post("/paint/liters", controllerManager.PaintController.ReturnLiters)
+
+
+}
